da43642448639874a7c4d24dca2dc8af7200d1ff
Ruben de Vries <r.devries6@students.uu.nl>
2019-06-14 12:22:34 +0200

    Added interpreter terminal in lambda_calculus.py

--- lambda_calculus.py
+++ lambda_calculus.py
@@ -1,7 +1,16 @@
 #!/usr/bin/env python3
 import re
+import sys
+"""
+Lambda Calculus Interpreter by Ruben de Vries (6440916).
+"""
 
-# TODO: finish lazy order reduciton.
+"""TODO:
+    1. Finish lazy order reduction.
+    2. Clean up code
+    3. Optimize code
+    4. Add types
+"""
 
 class LambdaTerm:
     """Abstract Base Class for lambda terms."""
@@ -630,19 +639,95 @@ LAMBDA_EXPS.extend(aliases + [(r"[A-Za-z]+[A-Za-z0-9]*", True)])
 LAMBDA_LEXER = Lexer(LAMBDA_EXPS)
 
 # =============================================================================
-# Testing code
+# Lambda Calculus Interpreter
 # =============================================================================
 
 if __name__ == "__main__":
-    XOR2 = Abstraction(Abstraction(Application(Application(AND, Application(Application(OR, x0), x1)), Application(NOT, Application(Application(AND, x1), x0)))))
+    # Beta reduces file if file path is given in arguments
+    if len(sys.argv) > 1:
+        with open(sys.argv[1]) as file:
+            content = file.read()
+        success, terms = from_string(content)
+        while success:
+            success, terms = terms.beta_reduce()
+        print(terms)
+        sys.exit(0)
 
-    flag, test = from_string(r"id xor")
+    class Command():
+        """Simple command object. For example, typing 'close' in terminal will
+        close this program.
 
-    if flag:
-        tag = True
-        while tag:
-            print(test.to_string())
-            tag, test = test.beta_reduce(NORMAL)
-    else:
-        print("Something went wrong!")
-        print(test)
+        Attributes:
+            name : the actual name the command is called by.
+            description : a description of the command which shows up when using
+                the 'help' command.
+            function : the function called when the command is executed.
+        """
+
+        def __init__(self, name, description, function):
+            self.name = name
+            self.description = description
+            self.function = function
+
+        def __call__(self):
+            """Executes command."""
+            return self.function()
+
+
+    def init():
+        """Initialize interpreter."""
+        print("Lambda calculus interpreter by Ruben de Vries(6440916).")
+        print("Type 'help' to find out how it works!")
+
+
+    def run():
+        """Run interpeter."""
+        global running, last
+        running = True
+
+        while running:
+            s = input("> ")
+
+            # Check if input corresponds with existing command
+            command_input = False
+            for command in COMMANDS:
+                if s == command.name:
+                    command()
+                    command_input = True
+                    break
+            if command_input:
+                continue
+
+            # Translating input to lambda term and reducing it to simplest form
+            s = s.replace("last", "(" + last + ")")
+            tag, lambda_term = from_string(s)
+            while lambda_term != None:
+                last = str(lambda_term)
+                print(last)
+                lambda_term = lambda_term.beta_reduce()
+
+
+    def command_info():
+        """Reveal all commands and their function."""
+        print("")
+        for command in COMMANDS:
+            print(command.description)
+
+
+    def close():
+        """Closes interpreter."""
+        print("\nClosing program ...")
+        global running
+        running = False
+
+
+    last = ""
+    running = False
+    COMMANDS = [
+            Command("help", "help : reveals all commands and their function.",
+                    command_info),
+            Command("close", "close : close interpreter.", close)]
+
+
+    init()
+    run()
